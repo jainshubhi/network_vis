bdc4f0c58d15090548ae07acced7415c2859ad2c
Shubhi Jain <shubhijain@caltech.edu>
2016-03-04 02:33:49 -0800

    Just making a basic change

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def index():
     return 'Hello Graph Vis!'
 
+@app.route('/<graph_name>')
+def load_graph(graph_name):
+    return 'You have just loaded %s!' % graph_name
+
 if __name__ == '__main__':
     app.run()
